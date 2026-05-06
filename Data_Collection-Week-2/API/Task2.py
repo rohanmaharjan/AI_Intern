589a763fa5f30faab4af46b56b7dcc142051d27e
Rohan Maharjan <rohanmhj9@gmail.com>
2026-05-06 18:31:29 +0545

    fixed issue file was opened but never used

--- Data_Collection-Week-2/API/Task2.py
+++ Data_Collection-Week-2/API/Task2.py
@@ -3,29 +3,35 @@ Fetch posts from the API.
 Save them to a CSV with columns: id, title, body.
 Then read the CSV back and print only posts where title contains more than 5 words.
 '''
-
+# corrected fields
+'''
+1. fixed issue where file was read but never used
+2. exception handling
+'''
 import requests
 import csv
 
 url = "https://jsonplaceholder.typicode.com/posts/"
-response = requests.get(url)
-
-if response.status_code == 200:
+try:
+    response = requests.get(url)
+    response.raise_for_status()
     posts = response.json()
 
+    # write csv
     with open("posts.csv", "w", newline="") as file:
-        writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
-        writer.writeheader()
+            writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+            writer.writeheader()
 
+            for post in posts:
+                writer.writerow({"id": post["id"], "title": post["title"], "body": post["body"]})
 
-        for post in posts:
-            writer.writerow({"id": post["id"], "title": post["title"], "body": post["body"]})
+    with open("posts.csv", "r") as file:
+         reader = csv.DictReader(file)
+         for row in reader:
+            words = row["title"].split()
+            if len(words)>5:
+                print(f'Id: {post["id"]} \t\t Title: {post["title"]} \t\t Body: {post["body"]}')
+        
 
-with open("posts.csv", "r") as file:
-    for post in posts:
-
-        title = post["title"]
-        words = title.split()
-        if len(words)>5:
-            print(f'Id: {post["id"]} \t\t Title: {post["title"]} \t\t Body: {post["body"]}')
-
+except Exception as e:
+    print("Unexpected error:", e)
